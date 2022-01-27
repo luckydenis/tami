407633de797f4ac141318b38ae61424445ee8424
Denis Belavin <41421345+LuckyDenis@users.noreply.github.com>
2022-01-27 23:48:44 +0300

    init stack

---
+++ structs/stack.py
@@ -0,0 +1,19 @@
+"""
+Реализовать структуру данных стек
+(FIFO - first in first out)
+На основе list.
+"""
+
+
+class Stack:
+    def __init__(self):
+        pass
+
+    def push(self, item):
+        pass
+
+    def pop(self):
+        pass
+
+    def contains(self, item):
+        pass
